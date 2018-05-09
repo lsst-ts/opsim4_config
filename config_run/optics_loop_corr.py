6fbf539f6181374c0fc2e5a011f47618ec7ded40
oboberg <boberg37@gmail.com>
2018-05-09 12:56:16 -0700

    baseline with dome crawl

--- config_run/optics_loop_corr.py
+++
@@ -1,3 +0,0 @@
-import lsst.sims.ocs.configuration.instrument.optics_loop_corr
-assert type(config)==lsst.sims.ocs.configuration.instrument.optics_loop_corr.OpticsLoopCorr, 'config is of type %s.%s instead of lsst.sims.ocs.configuration.instrument.optics_loop_corr.OpticsLoopCorr' % (type(config).__module__, type(config).__name__)
-config.tel_optics_cl_delay=[0.0, 36.0]
