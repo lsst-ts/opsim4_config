4e3feb2224f9249bbcfd6bd5ae0e3c0e025a9ae8
oboberg <boberg37@gmail.com>
2018-05-10 15:06:34 -0700

    config for big wfd only, panstarrs like

--- config_run/galacticplane_prop.py
+++
@@ -1,5 +0,0 @@
-import lsst.sims.ocs.configuration.science.galactic_plane
-assert type(config)==lsst.sims.ocs.configuration.science.galactic_plane.GalacticPlane, 'config is of type %s.%s instead of lsst.sims.ocs.configuration.science.galactic_plane.GalacticPlane' % (type(config).__module__, type(config).__name__)
-config.scheduling.airmass_bonus=0.0
-config.scheduling.hour_angle_bonus=0.3
-config.scheduling.hour_angle_max=3.0
--- config_run/widefastdeep_prop.py
+++ config_run/widefastdeep_prop.py
@@ -1,5 +1,9 @@
 import lsst.sims.ocs.configuration.science.wide_fast_deep
 assert type(config)==lsst.sims.ocs.configuration.science.wide_fast_deep.WideFastDeep, 'config is of type %s.%s instead of lsst.sims.ocs.configuration.science.wide_fast_deep.WideFastDeep' % (type(config).__module__, type(config).__name__)
+config.sky_region.selections[0].minimum_limit=-78.0
+config.sky_region.selections[0].maximum_limit=18.0
 config.scheduling.airmass_bonus=0.0
 config.scheduling.hour_angle_bonus=0.3
 config.scheduling.hour_angle_max=3.0
+config.sky_exclusion.dec_window=90.0
+config.sky_exclusion.selections={}
