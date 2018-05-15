f9aabfa7e4bea8ba0162122fcaa6cb76d9939b78
oboberg <boberg37@gmail.com>
2018-05-15 09:28:06 -0700

    wfd cadence through gp, no gp proposal

---
+++ config_run/survey.py
@@ -0,0 +1,3 @@
+import lsst.sims.ocs.configuration.survey
+assert type(config)==lsst.sims.ocs.configuration.survey.Survey, 'config is of type %s.%s instead of lsst.sims.ocs.configuration.survey.Survey' % (type(config).__module__, type(config).__name__)
+config.general_proposals=['NorthEclipticSpur', 'SouthCelestialPole', 'WideFastDeep']
