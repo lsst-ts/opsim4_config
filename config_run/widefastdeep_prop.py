b72e944bbf47c0d846f48a9e047156c7d5d73965
oboberg <boberg37@gmail.com>
2018-05-15 08:38:23 -0700

    fix scp and wfd for no pairs

--- config_run/widefastdeep_prop.py
+++ config_run/widefastdeep_prop.py
@@ -3,6 +3,8 @@ assert type(config)==lsst.sims.ocs.configuration.science.wide_fast_deep.WideFast
 config.scheduling.airmass_bonus=0.0
 config.scheduling.hour_angle_bonus=0.3
 config.scheduling.hour_angle_max=3.0
+config.filters['u'].num_grouped_visits=1
+config.filters['u'].max_grouped_visits=1
 config.filters['g'].num_grouped_visits=1
 config.filters['g'].max_grouped_visits=1
 config.filters['r'].num_grouped_visits=1
