f5967f24d5c8a581333b2147487bedfe1a0e5f3d
oboberg <boberg37@gmail.com>
2018-05-15 08:36:56 -0700

    fix proposals

--- config_run/northeclipticspur_prop.py
+++ config_run/northeclipticspur_prop.py
@@ -3,9 +3,12 @@ assert type(config)==lsst.sims.ocs.configuration.science.north_ecliptic_spur.Nor
 config.scheduling.airmass_bonus=0.0
 config.scheduling.hour_angle_bonus=0.3
 config.scheduling.hour_angle_max=3.0
+config.filters['g'].num_grouped_visits=1
+config.filters['g'].max_grouped_visits=1
 config.filters['r'].num_grouped_visits=1
 config.filters['r'].max_grouped_visits=1
 config.filters['i'].num_grouped_visits=1
 config.filters['i'].max_grouped_visits=1
 config.filters['z'].num_grouped_visits=1
 config.filters['z'].max_grouped_visits=1
+config.filters['y'].max_grouped_visits=1
