21dfb6882230e08475d6fd2d4d28e564a062f37b
Tiago Ribeiro <tiago.astro@gmail.com>
2018-05-22 08:36:36 -0700

    Trying to fix adding of WideFastDeepRolling proposal.

--- config_run/survey.py
+++ config_run/survey.py
@@ -1,3 +1,5 @@
 import lsst.sims.ocs.configuration.survey
 assert type(config)==lsst.sims.ocs.configuration.survey.Survey, 'config is of type %s.%s instead of lsst.sims.ocs.configuration.survey.Survey' % (type(config).__module__, type(config).__name__)
-config.general_proposals=['GalacticPlane', 'NorthEclipticSpur', 'SouthCelestialPole']
+config.general_proposals=['GalacticPlane', 'NorthEclipticSpur', 'SouthCelestialPole',
+                          'WideFastDeep',
+                          'WideFastDeepRolling']
