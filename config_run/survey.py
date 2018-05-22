c23dc5e6c66bb8cad54f813b8f31034cfbfa3211
Tiago Ribeiro <tiago.astro@gmail.com>
2018-05-22 09:16:05 -0700

    trying to fix list of general proposals...

--- config_run/survey.py
+++ config_run/survey.py
@@ -2,4 +2,6 @@ import lsst.sims.ocs.configuration.survey
 assert type(config)==lsst.sims.ocs.configuration.survey.Survey, 'config is of type %s.%s instead of lsst.sims.ocs.configuration.survey.Survey' % (type(config).__module__, type(config).__name__)
 config.general_proposals=['GalacticPlane', 'NorthEclipticSpur', 'SouthCelestialPole',
                           'WideFastDeep',
-                          'WideFastDeepRolling']
+                          'WideFastDeepRollingAll',
+                          'WideFastDeepRollingS',
+                          'WideFastDeepRollingN']
