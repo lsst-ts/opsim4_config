18eec7e66dc3d776151876fbe9aa2019ef4bed17
oboberg <boberg37@gmail.com>
2018-05-25 15:32:21 -0700

    remove GP exclusion from SCP

--- config_run/southcelestialpole_prop.py
+++ config_run/southcelestialpole_prop.py
@@ -3,3 +3,5 @@ assert type(config)==lsst.sims.ocs.configuration.science.south_celestial_pole.So
 config.scheduling.airmass_bonus=0.0
 config.scheduling.hour_angle_bonus=0.3
 config.scheduling.hour_angle_max=3.0
+config.sky_exclusion.dec_window=90.0
+config.sky_exclusion.selections={}
