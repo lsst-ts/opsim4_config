df94e1cc904bb42060b43013039dfbe68562d420
oboberg <boberg37@gmail.com>
2018-06-13 11:20:33 -0700

    3 decbands in single new proposal

---
+++ config_run/new_props/wide_fast_deep_rolling.py
@@ -0,0 +1,251 @@
+import lsst.pex.config as pexConfig
+from lsst.sims.ocs.configuration.proposal import General, GeneralBandFilter, Selection, SelectionList, TimeRange
+from lsst.sims.ocs.configuration.proposal import general_prop_reg
+__all__ = ["WideFastDeepRolling"]
+@pexConfig.registerConfig("WideFastDeepRolling", general_prop_reg, General)
+class WideFastDeepRolling(General):
+    def setDefaults(self):
+        self.name = "WideFastDeepRolling"
+        # -------------------------
+        # Sky Region specifications
+        # -------------------------
+        #A0
+        sel0 = Selection()
+        sel0.limit_type = "Dec"
+        sel0.minimum_limit = -62.5
+        sel0.maximum_limit = 2.8
+        #N1
+        sel1 = Selection()
+        sel1.limit_type = "Dec"
+        sel1.minimum_limit = -15.2
+        sel1.maximum_limit = 2.8
+        #M2
+        sel2 = Selection()
+        sel2.limit_type = "Dec"
+        sel2.minimum_limit = -35.0
+        sel2.maximum_limit = -15.2
+        #S3
+        sel3 = Selection()
+        sel3.limit_type = "Dec"
+        sel3.minimum_limit = -62.5
+        sel3.maximum_limit = -35.0
+        #N4
+        sel4 = Selection()
+        sel4.limit_type = "Dec"
+        sel4.minimum_limit = -15.2
+        sel4.maximum_limit = 2.8
+        #M5
+        sel5 = Selection()
+        sel5.limit_type = "Dec"
+        sel5.minimum_limit = -35.0
+        sel5.maximum_limit = -15.2
+        #S6
+        sel6 = Selection()
+        sel6.limit_type = "Dec"
+        sel6.minimum_limit = -62.5
+        sel6.maximum_limit = -35.0
+        #N7
+        sel7 = Selection()
+        sel7.limit_type = "Dec"
+        sel7.minimum_limit = -15.2
+        sel7.maximum_limit = 2.8
+        #M8
+        sel8 = Selection()
+        sel8.limit_type = "Dec"
+        sel8.minimum_limit = -35.0
+        sel8.maximum_limit = -15.2
+        #S9
+        sel9 = Selection()
+        sel9.limit_type = "Dec"
+        sel9.minimum_limit = -62.5
+        sel9.maximum_limit = -35.0
+        #A10
+        sel10 = Selection()
+        sel10.limit_type = "Dec"
+        sel10.minimum_limit = -62.5
+        sel10.maximum_limit = 2.8
+        self.sky_region.selections = {0: sel0,
+                                      1: sel1,
+                                      2: sel2,
+                                      3: sel3,
+                                      4: sel4,
+                                      5: sel5,
+                                      6: sel6,
+                                      7: sel7,
+                                      8: sel8,
+                                      9: sel9,
+                                      10: sel10}
+        time_range0 = TimeRange()
+        time_range0.start = 1
+        time_range0.end = 365
+        time_range1 = TimeRange()
+        time_range1.start = 366
+        time_range1.end = 690
+        time_range2 = TimeRange()
+        time_range2.start = 691
+        time_range2.end = 1015
+        time_range3 = TimeRange()
+        time_range3.start = 1016
+        time_range3.end = 1340
+        time_range4 = TimeRange()
+        time_range4.start = 1341
+        time_range4.end = 1665
+        time_range5 = TimeRange()
+        time_range5.start = 1666
+        time_range5.end = 1990
+        time_range6 = TimeRange()
+        time_range6.start = 1991
+        time_range6.end = 2315
+        time_range7 = TimeRange()
+        time_range7.start = 2316
+        time_range7.end = 2639
+        time_range8 = TimeRange()
+        time_range8.start = 2640
+        time_range8.end = 2963
+        time_range9 = TimeRange()
+        time_range9.start = 2964
+        time_range9.end = 3287
+        time_range10 = TimeRange()
+        time_range10.start = 3288
+        time_range10.end = 3650
+        self.sky_region.time_ranges = {0: time_range0,
+                                       1: time_range1,
+                                       2: time_range2,
+                                       3: time_range3,
+                                       4: time_range4,
+                                       5: time_range5,
+                                       6: time_range6,
+                                       7: time_range7,
+                                       8: time_range8,
+                                       9: time_range9,
+                                       10: time_range10}
+        sel_map0 = SelectionList()
+        sel_map0.indexes = [0]
+        sel_map1 = SelectionList()
+        sel_map1.indexes = [1]
+        sel_map2 = SelectionList()
+        sel_map2.indexes = [2]
+        sel_map3 = SelectionList()
+        sel_map3.indexes = [3]
+        sel_map4 = SelectionList()
+        sel_map4.indexes = [4]
+        sel_map5 = SelectionList()
+        sel_map5.indexes = [5]
+        sel_map6 = SelectionList()
+        sel_map6.indexes = [6]
+        sel_map7 = SelectionList()
+        sel_map7.indexes = [7]
+        sel_map8 = SelectionList()
+        sel_map8.indexes = [8]
+        sel_map9 = SelectionList()
+        sel_map9.indexes = [9]
+        sel_map10 = SelectionList()
+        sel_map10.indexes = [10]
+        self.sky_region.selection_mapping = {0: sel_map0,
+                                             1: sel_map1,
+                                             2: sel_map2,
+                                             3: sel_map3,
+                                             4: sel_map4,
+                                             5: sel_map5,
+                                             6: sel_map6,
+                                             7: sel_map7,
+                                             8: sel_map8,
+                                             9: sel_map9,
+                                             10: sel_map10}
+
+        # ----------------------------
+        # Sky Exclusion specifications
+        # ----------------------------
+        self.sky_exclusion.dec_window = 90.0
+        excl0 = Selection()
+        excl0.limit_type = "GP"
+        excl0.minimum_limit = 0.0
+        excl0.maximum_limit = 10.0
+        excl0.bounds_limit = 90.0
+        self.sky_exclusion.selections = {0: excl0}
+        # ---------------------------------
+        # Sky Nightly Bounds specifications
+        # ---------------------------------
+        self.sky_nightly_bounds.twilight_boundary = -12.0
+        self.sky_nightly_bounds.delta_lst = 60.0
+        # ------------------------------
+        # Sky Constraints specifications
+        # ------------------------------
+        self.sky_constraints.max_airmass = 1.5
+        self.sky_constraints.max_cloud = 0.7
+        # ----------------------
+        # Scheduling information
+        # ----------------------
+        self.scheduling.max_num_targets = 500
+        self.scheduling.accept_serendipity = False
+        self.scheduling.accept_consecutive_visits = False
+        self.scheduling.restrict_grouped_visits = True
+        self.scheduling.time_interval = 1800
+        self.scheduling.time_window_start = .5
+        self.scheduling.time_window_max = 1.0
+        self.scheduling.time_window_end = 2.0
+        self.scheduling.time_weight = 1.0
+        self.sky_constraints.max_cloud = 0.7
+        # ----------------
+        # HA and X bonuses
+        # ----------------
+        self.scheduling.airmass_bonus=0.0
+        self.scheduling.hour_angle_bonus=0.3
+        self.scheduling.hour_angle_max=3.0
+        # --------------------------
+        # Band Filter specifications
+        # --------------------------
+        u_filter = GeneralBandFilter()
+        u_filter.name = 'u'
+        u_filter.num_visits = 15
+        u_filter.num_grouped_visits = 1
+        u_filter.bright_limit = 21.3
+        u_filter.dark_limit = 30.0
+        u_filter.max_seeing = 1.5
+        u_filter.exposures = [15.0, 15.0]
+        g_filter = GeneralBandFilter()
+        g_filter.name = 'g'
+        g_filter.num_visits = 21
+        g_filter.num_grouped_visits = 2
+        g_filter.bright_limit = 21.0
+        g_filter.dark_limit = 30.0
+        g_filter.max_seeing = 1.5
+        g_filter.exposures = [15.0, 15.0]
+        r_filter = GeneralBandFilter()
+        r_filter.name = 'r'
+        r_filter.num_visits = 48
+        r_filter.num_grouped_visits = 2
+        r_filter.bright_limit = 20.25
+        r_filter.dark_limit = 30.0
+        r_filter.max_seeing = 1.5
+        r_filter.exposures = [15.0, 15.0]
+        i_filter = GeneralBandFilter()
+        i_filter.name = 'i'
+        i_filter.num_visits = 48
+        i_filter.num_grouped_visits = 2
+        i_filter.bright_limit = 19.5
+        i_filter.dark_limit = 30.0
+        i_filter.max_seeing = 1.5
+        i_filter.exposures = [15.0, 15.0]
+        z_filter = GeneralBandFilter()
+        z_filter.name = 'z'
+        z_filter.num_visits = 42
+        z_filter.num_grouped_visits = 2
+        z_filter.bright_limit = 17.0
+        z_filter.dark_limit = 21.0
+        z_filter.max_seeing = 1.5
+        z_filter.exposures = [15.0, 15.0]
+        y_filter = GeneralBandFilter()
+        y_filter.name = 'y'
+        y_filter.num_visits = 42
+        y_filter.num_grouped_visits = 1
+        y_filter.bright_limit = 16.5
+        y_filter.dark_limit = 21.0
+        y_filter.max_seeing = 1.5
+        y_filter.exposures = [15.0, 15.0]
+        self.filters = {u_filter.name: u_filter,
+                        g_filter.name: g_filter,
+                        r_filter.name: r_filter,
+                        i_filter.name: i_filter,
+                        z_filter.name: z_filter,
+                        y_filter.name: y_filter}
--- config_run/survey.py
+++ config_run/survey.py
@@ -1,14 +1,3 @@
 import lsst.sims.ocs.configuration.survey
 assert type(config)==lsst.sims.ocs.configuration.survey.Survey, 'config is of type %s.%s instead of lsst.sims.ocs.configuration.survey.Survey' % (type(config).__module__, type(config).__name__)
-config.general_proposals=['GalacticPlane', 'NorthEclipticSpur', 'SouthCelestialPole',
-                          'WideFastDeepRollingAll0',
-                          'WideFastDeepRollingN1',
-                          'WideFastDeepRollingM2',
-                          'WideFastDeepRollingS3',
-                          'WideFastDeepRollingN4',
-                          'WideFastDeepRollingM5',
-                          'WideFastDeepRollingS6',
-                          'WideFastDeepRollingN7',
-                          'WideFastDeepRollingM8',
-                          'WideFastDeepRollingS9',
-                          'WideFastDeepRollingAll10']
+config.general_proposals=['GalacticPlane', 'NorthEclipticSpur', 'SouthCelestialPole','WideFastDeepRolling']
