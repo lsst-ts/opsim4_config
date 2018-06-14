070b1cf26ef191e4f8e0b5d87fc19088b6ac8686
oboberg <boberg37@gmail.com>
2018-06-13 19:11:07 -0700

    add list of 9 fields for ddfs

--- config_run/deepdrillingcosmology1_prop.py
+++ config_run/deepdrillingcosmology1_prop.py
@@ -1,4 +1,5 @@
 import lsst.sims.ocs.configuration.science.deep_drilling_cosmology1
 assert type(config)==lsst.sims.ocs.configuration.science.deep_drilling_cosmology1.DeepDrillingCosmology1, 'config is of type %s.%s instead of lsst.sims.ocs.configuration.science.deep_drilling_cosmology1.DeepDrillingCosmology1' % (type(config).__module__, type(config).__name__)
+config.sky_user_regions=[290, 744, 820, 858, 1200, 1427, 2412, 2689, 2786]
 config.scheduling.airmass_bonus=0.0
 config.scheduling.hour_angle_bonus=0.3
