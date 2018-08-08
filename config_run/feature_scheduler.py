27039814a46061b02d04df0c2691042e5e1e1f6e
Tiago Ribeiro <tiago.astro@gmail.com>
2018-08-07 23:37:19 -0700

    Full config simulation. All filters, pairs and DD.

--- config_run/feature_scheduler.py
+++ config_run/feature_scheduler.py
@@ -23,16 +23,44 @@ if __name__ == 'config':
     target_maps = {}
     nside = fs.set_default_nside(nside=32)  # Required
 
-    target_maps['i'] = fs.generate_goal_map(NES_fraction=0.1,
-                                            WFD_fraction=.8, SCP_fraction=0.05,
-                                            GP_fraction=.05,
+    target_maps['u'] = fs.generate_goal_map(NES_fraction=0.,
+                                            WFD_fraction=.043, SCP_fraction=0.017,
+                                            GP_fraction=.017,
                                             WFD_upper_edge_fraction=0.0,
                                             nside=nside,
                                             generate_id_map=True)
 
-    target_maps['r'] = fs.generate_goal_map(NES_fraction=0.1,
-                                            WFD_fraction=.8, SCP_fraction=0.05,
-                                            GP_fraction=.05,
+    target_maps['g'] = fs.generate_goal_map(NES_fraction=0.023,
+                                            WFD_fraction=.06, SCP_fraction=0.017,
+                                            GP_fraction=.017,
+                                            WFD_upper_edge_fraction=0.0,
+                                            nside=nside,
+                                            generate_id_map=True)
+
+    target_maps['r'] = fs.generate_goal_map(NES_fraction=0.023,
+                                            WFD_fraction=0.14, SCP_fraction=0.017,
+                                            GP_fraction=.017,
+                                            WFD_upper_edge_fraction=0.0,
+                                            nside=nside,
+                                            generate_id_map=True)
+
+    target_maps['i'] = fs.generate_goal_map(NES_fraction=0.052,
+                                            WFD_fraction=.14, SCP_fraction=0.017,
+                                            GP_fraction=.017,
+                                            WFD_upper_edge_fraction=0.0,
+                                            nside=nside,
+                                            generate_id_map=True)
+
+    target_maps['z'] = fs.generate_goal_map(NES_fraction=0.046,
+                                            WFD_fraction=.12, SCP_fraction=0.017,
+                                            GP_fraction=.017,
+                                            WFD_upper_edge_fraction=0.0,
+                                            nside=nside,
+                                            generate_id_map=True)
+
+    target_maps['y'] = fs.generate_goal_map(NES_fraction=0.0,
+                                            WFD_fraction=.12, SCP_fraction=0.017,
+                                            GP_fraction=.017,
                                             WFD_upper_edge_fraction=0.0,
                                             nside=nside,
                                             generate_id_map=True)
@@ -83,21 +111,18 @@ if __name__ == 'config':
     #                 'az_min': 180. - az, 'az_max': 180. + az,
     #                 'weight': .9})
 
-    # filters = ['u', 'g', 'r', 'i', 'z', 'y']
-    filters = ['r', 'i']
+    filters = ['u', 'g', 'r', 'i', 'z', 'y']
+    # filters = ['r', 'i']
     surveys = []
 
     sb_limit_map = fs.utils.generate_sb_map(target_maps, filters)
 
-    filter_prop = {'u': 0.069,
-                   'g': 0.097,
-                   'r': 0.222,
-                   'i': 0.222,
-                   'z': 0.194,
-                   'y': 0.194}
-
-    filter_prop = {'r': 0.5,
-                   'i': 0.5}
+    filter_prop = {'u': 0.0774,
+                   'g': 0.117,
+                   'r': 0.224,
+                   'i': 0.224,
+                   'z': 0.200,
+                   'y': 0.154}
 
     for filtername in filters:
         bfs = list()
@@ -142,5 +167,98 @@ if __name__ == 'config':
                                                tag_map=target_maps[filtername][1],
                                                tag_names=target_maps[filtername][2],
                                                ignore_obs='DD'))
+
+    # Set up pairs
+    pairs_bfs = []
+
+    pair_map = np.zeros(len(target_maps['z'][0]))
+    pair_map.fill(hp.UNSEEN)
+    wfd = np.where(target_maps['z'][1] == 3)
+    nes = np.where(target_maps['z'][1] == 1)
+    pair_map[wfd] = 1.
+    pair_map[nes] = 1.
+
+    pairs_bfs.append(fs.Target_map_basis_function(filtername='',
+                                                  target_map=pair_map,
+                                                  out_of_bounds_val=hp.UNSEEN, nside=nside))
+    pairs_bfs.append(fs.MeridianStripeBasisFunction(nside=nside, zenith_pad=(45.,), width=(35.,)))
+    pairs_bfs.append(fs.Moon_avoidance_basis_function(nside=nside, moon_distance=30.))
+
+    surveys.append(fs.Pairs_survey_scripted(pairs_bfs, [1., 1., 1.], ignore_obs='DD', min_alt=20.))
+    # surveys.append(fs.Pairs_different_filters_scripted(pairs_bfs, [1., 1., 1.], ignore_obs='DD', min_alt=20.,
+    #                                                    filter_goals=filter_prop))
+    # surveys.append(fs.Pairs_survey_scripted([], [], ignore_obs='DD'))
+
+    # Set up the DD
+    # ELAIS S1
+    surveys.append(fs.Deep_drilling_survey(9.45, -44., sequence='rgizy',
+                                           nvis=[20, 10, 20, 26, 20],
+                                           survey_name='DD:ELAISS1', reward_value=100, moon_up=None,
+                                           fraction_limit=0.148, ha_limits=([0., 0.5], [23.5, 24.]),
+                                           nside=nside,
+                                           avoid_same_day=True,
+                                           filter_goals=filter_prop))
+    surveys.append(fs.Deep_drilling_survey(9.45, -44., sequence='u',
+                                           nvis=[7],
+                                           survey_name='DD:u,ELAISS1', reward_value=100, moon_up=False,
+                                           fraction_limit=0.0012, ha_limits=([0., 0.5], [23.5, 24.]),
+                                           nside=nside))
+
+    # XMM-LSS
+    surveys.append(fs.Deep_drilling_survey(35.708333, -4 - 45 / 60., sequence='rgizy',
+                                           nvis=[20, 10, 20, 26, 20],
+                                           survey_name='DD:XMM-LSS', reward_value=100, moon_up=None,
+                                           fraction_limit=0.148, ha_limits=([0., 0.5], [23.5, 24.]),
+                                           nside=nside,
+                                           avoid_same_day=True,
+                                           filter_goals=filter_prop))
+    surveys.append(fs.Deep_drilling_survey(35.708333, -4 - 45 / 60., sequence='u',
+                                           nvis=[7],
+                                           survey_name='DD:u,XMM-LSS', reward_value=100, moon_up=False,
+                                           fraction_limit=0.0012, ha_limits=([0., 0.5], [23.5, 24.]),
+                                           nside=nside))
+
+    # Extended Chandra Deep Field South
+    # XXX--Note, this one can pass near zenith. Should go back and add better planning on this.
+    surveys.append(fs.Deep_drilling_survey(53.125, -28. - 6 / 60., sequence='rgizy',
+                                           nvis=[20, 10, 20, 26, 20],
+                                           survey_name='DD:ECDFS', reward_value=100, moon_up=None,
+                                           fraction_limit=0.148, ha_limits=[[0.5, 1.0], [23., 22.5]],
+                                           nside=nside,
+                                           avoid_same_day=True,
+                                           filter_goals=filter_prop))
+    surveys.append(fs.Deep_drilling_survey(53.125, -28. - 6 / 60., sequence='u',
+                                           nvis=[7],
+                                           survey_name='DD:u,ECDFS', reward_value=100, moon_up=False,
+                                           fraction_limit=0.0012, ha_limits=[[0.5, 1.0], [23., 22.5]],
+                                           nside=nside))
+    # COSMOS
+    surveys.append(fs.Deep_drilling_survey(150.1, 2. + 10. / 60. + 55 / 3600., sequence='rgizy',
+                                           nvis=[20, 10, 20, 26, 20],
+                                           survey_name='DD:COSMOS', reward_value=100, moon_up=None,
+                                           fraction_limit=0.148, ha_limits=([0., 0.5], [23.5, 24.]),
+                                           nside=nside,
+                                           avoid_same_day=True,
+                                           filter_goals=filter_prop))
+    surveys.append(fs.Deep_drilling_survey(150.1, 2. + 10. / 60. + 55 / 3600., sequence='u',
+                                           nvis=[7], ha_limits=([0., .5], [23.5, 24.]),
+                                           survey_name='DD:u,COSMOS', reward_value=100, moon_up=False,
+                                           fraction_limit=0.0012,
+                                           nside=nside))
+
+    # Extra DD Field, just to get to 5. Still not closed on this one
+    surveys.append(fs.Deep_drilling_survey(349.386443, -63.321004, sequence='rgizy',
+                                           nvis=[20, 10, 20, 26, 20],
+                                           survey_name='DD:290', reward_value=100, moon_up=None,
+                                           fraction_limit=0.148, ha_limits=([0., 0.5], [23.5, 24.]),
+                                           nside=nside,
+                                           avoid_same_day=True,
+                                           filter_goals=filter_prop))
+    surveys.append(fs.Deep_drilling_survey(349.386443, -63.321004, sequence='u',
+                                           nvis=[7],
+                                           survey_name='DD:u,290', reward_value=100, moon_up=False,
+                                           fraction_limit=0.0012, ha_limits=([0., 0.5], [23.5, 24.]),
+                                           nside=nside,
+                                           filter_goals=filter_prop))
 
     scheduler = fs.Core_scheduler(surveys, nside=nside)  # Required
