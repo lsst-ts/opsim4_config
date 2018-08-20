8b970c6f7f369b50fa50f71ad05119b03b6a7094
Tiago Ribeiro <tiago.astro@gmail.com>
2018-08-20 14:24:06 -0700

    Using pairs in filter gri instead of griz.

--- config_run/feature_scheduler.py
+++ config_run/feature_scheduler.py
@@ -147,7 +147,8 @@ if __name__ == 'config':
     pairs_bfs.append(fs.MeridianStripeBasisFunction(nside=nside, zenith_pad=(45.,), width=(35.,)))
     pairs_bfs.append(fs.Moon_avoidance_basis_function(nside=nside, moon_distance=30.))
 
-    surveys.append(fs.Pairs_survey_scripted(pairs_bfs, [1., 1., 1.], ignore_obs='DD', min_alt=20.))
+    surveys.append(fs.Pairs_survey_scripted(pairs_bfs, [1., 1., 1.], ignore_obs='DD', min_alt=20.,
+                                            filt_to_pair='gri'))
     # surveys.append(fs.Pairs_different_filters_scripted(pairs_bfs, [1., 1., 1.], ignore_obs='DD', min_alt=20.,
     #                                                    filter_goals=filter_prop))
     # surveys.append(fs.Pairs_survey_scripted([], [], ignore_obs='DD'))
